a031ae5cfa910d23566ad0ef279b984d4b96bbe0
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2020-11-23 04:09:23 +0900

    Add files via upload

---
+++ 21_bbc_20203117.ino
@@ -0,0 +1,69 @@
+#include <Servo.h>
+
+//Arduino pin assignment
+#define PIN_SERVO 10
+#define PIN_IR A0
+
+//Framework setting
+#define _DIST_MIN 100
+#define _DIST_MAX 410
+
+//Distance sensor
+#define _DIST_ALPHA 0.0
+
+//Servo range
+#define _DUTY_MIN 500
+#define _DUTY_NEU 1480
+#define _DUTY_MAX 2400
+
+#define _DIST_ALPHA 0.0
+
+#define _SERVO_ANGLE 30.0
+#define _SERVO_SPEED 30.0
+
+//Servo instance
+Servo myservo;
+int a, b;
+
+float dist_raw, dist_ema;
+
+int duty_chg_per_interval;
+
+void setup(){
+//attach servo
+  myservo.attach(PIN_SERVO);
+
+//move servo to neutral position
+  myservo.writeMicroseconds(_DUTY_NEU);
+
+//initialize serial port
+  Serial.begin(57600);
+
+  a = 70;
+  b = 390;
+  myservo.writeMicroseconds(1480);
+}
+
+float ir_distance(void){ // return value unit: mm
+  float val;
+  float volt = float(analogRead(PIN_IR));
+  val = ((6762.0/(volt-9.0))-4.0) * 10.0;
+  return val;
+}
+
+void loop(){
+  float raw_dist = ir_distance();
+  float dist_cali = 100 + 300.0 / (b - a) * (raw_dist - a);
+  Serial.print("min:100, max:410, dist:");
+  Serial.print(raw_dist);
+  Serial.print(",dist_cali:");
+  Serial.println(dist_cali);
+  
+  if(dist_cali > 255) {
+    myservo.writeMicroseconds(900);
+  }
+  else {
+    myservo.writeMicroseconds(1480);
+  }
+  delay(20);
+}
